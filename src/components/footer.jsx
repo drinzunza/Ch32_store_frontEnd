bbe78ce11547e8712f61fcd2d0fd780f5ee8d9e3
Sergio Inzunza <sinzunza@sdgku.edu>
2022-11-08 10:04:23 -0700

    FSDI 107 #3

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+function Footer() {
+  return (
+    <div className="footer">
+      <label>2022</label>
+      <div className="name">Organika Store - Fresh food 1 click away.</div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/product.jsx
@@ -0,0 +1,30 @@
+import './product.css';
+import QuantityPicker from './quantityPicker';
+
+function Product(props) {
+  return (
+    <div className="product">
+      <img src={'/images/' + props.data.image} alt="" />
+
+      <h5>{props.data.title}</h5>
+
+      <div className="prices">
+        <label>${props.data.price}</label>
+        <label>${props.data.price}</label>
+      </div>
+
+      <QuantityPicker />
+
+      <button className="btn btn-sm btn-info">Add</button>
+    </div>
+  );
+}
+
+export default Product;
+
+/**
+ * create a QuantityPicker component
+ *  with button / label / button
+ *
+ * Render QuantityPicker on Product component
+ */
